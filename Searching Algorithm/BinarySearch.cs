bbc908049820c040c031a27e55686d72acb8465a
Hisham Abdalla <hishvmabdalla@gmail.com>
2024-02-16 19:17:25 +0200

    Searching Algorithm

---
+++ Searching Algorithm/BinarySearch.cs
@@ -0,0 +1,54 @@
+﻿namespace BinarySearch
+{
+    internal class Program
+    {
+        static int  BinarySearch(int[]arr,int l,int h,int key)
+        {
+            while (l<=h)
+            {
+                int m = (l + h) / 2;
+                if (arr[m] == key)
+                {
+                    return m;
+                }
+                else if (arr[m] > key)
+                {
+                    h = m - 1;
+                }
+                else
+                {
+                    l= m + 1;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Sort Numbers:");
+            string[] input = Console.ReadLine().Split(" ");
+
+            int size = input.Length;
+
+            int[]arr = new int[size];
+            Console.WriteLine("Enter the Integer: ");
+            int key=int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = int.Parse(input[i]);
+            }
+
+            int Result=BinarySearch(arr, 0, arr.Length-1,key);
+
+            if (Result == -1)
+            {
+                Console.WriteLine($"The Number : ( {key} ) Was Not Found.");
+            }
+            else
+            {
+                Console.WriteLine($"The Number: ( {arr[Result]} ) Was Found in Index ( {Result} )");
+            }
+
+        }
+    }
+}
---
+++ Searching Algorithm/LinearSearch.cs
@@ -0,0 +1,47 @@
+﻿namespace linear_Search
+{
+    internal class Program
+    {
+        public static int LinearSearch(int[] arr,int size,int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                if (arr[i] == num)
+                {
+                    return i;
+                    
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter The Numbers Of Array: ");
+            string[] arr =Console.ReadLine().Split(" ");
+
+            int Size = arr.Length;
+
+            int[] arrOfNum = new int[Size];
+            
+            for (int i = 0;i < arr.Length;i++)
+            {
+                arrOfNum[i] = int.Parse(arr[i]);
+            }
+            Console.WriteLine("Enter the integer");
+            int Num = int.Parse(Console.ReadLine());
+
+            int Result = LinearSearch(arrOfNum, Size, Num);
+
+           if(Result == -1) 
+           {
+                Console.WriteLine($"The Number : ( {Num} ) Was Not Found.");
+           }
+
+           else
+           {
+                Console.WriteLine($"The Number : ( {arrOfNum[Result]} ) Was  Found in index ( {Result} )");
+           }
+        }
+    }
+}
